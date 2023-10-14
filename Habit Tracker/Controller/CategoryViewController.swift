fb30473340710b92a23f66b4c80b7748e60b23f7
Dennis Münchow <dennis.muenchow@googlemail.com>
2023-10-14 13:59:21 +0200

    first commit

---
+++ Habit Tracker/Controller/CategoryViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Habit Tracker
+//
+//  Created by Dennis Münchow on 14.10.23.
+//
+
+import UIKit
+
+class CategoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
---
+++ Habit Tracker/Controller/RelationshipController.swift
@@ -0,0 +1,28 @@
+//
+//  RelationshipController.swift
+//  Habit Tracker
+//
+//  Created by Dennis Münchow on 14.10.23.
+//
+
+import UIKit
+
+class RelationshipController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    
+}
