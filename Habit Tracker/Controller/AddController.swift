51c80ffdd35a84f7c63129c104c4398107055ea1
Dennis Münchow <dennis.muenchow@googlemail.com>
2023-10-17 21:20:10 +0200

    Modifying AddController

--- Habit Tracker/Controller/AddController.swift
+++ Habit Tracker/Controller/AddController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import CoreData
+
 
 class AddController: UIViewController, UITextFieldDelegate {
     
@@ -14,7 +16,10 @@ class AddController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var datePicker: UIDatePicker!
     
     public var completion : ((String, String, Date) -> Void)?
-    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var selectedCategory: String?
+    var itemArray = [Item]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         titleField.delegate = self
@@ -26,19 +31,35 @@ class AddController: UIViewController, UITextFieldDelegate {
            let noteText = noteField.text, !noteText.isEmpty {
             
             let targetDate = datePicker.date
-            print(targetDate)
-            //let targetTime = datePicker.time
+            
+            let newItem = Item(context: self.context)
+            newItem.title = titleText
+            newItem.done = false
+            newItem.category = self.selectedCategory
+            newItem.date = targetDate
+            
+            print(newItem.title, newItem.done, newItem.category)
+            
+            
+            self.itemArray.append(newItem)
+            self.saveItems()
+            
+            
+            dismiss(animated: true, completion: nil)
             
             completion?(titleText, noteText, targetDate)
-            
         }
     }
+    
     @IBAction func cancelPressed(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
     }
     
-
-    
-
-    
+    func saveItems () {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving contect \(error)")
+        }
+    }
 }
--- Habit Tracker/Controller/ItemController.swift
+++ Habit Tracker/Controller/ItemController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import CoreData
 
+
 class ItemController: UITableViewController {
     
     @IBOutlet weak var searchBar: UISearchBar!
@@ -15,14 +16,18 @@ class ItemController: UITableViewController {
     var itemArray = [Item]()
     let date: Date = Date(timeIntervalSinceReferenceDate: 625_000)
     
+    
     var selectedCategory: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = selectedCategory
-        
+                
         loadItems()
+        tableView.reloadData()
     }
+    
+
     
     // MARK: - Table view data source
     
@@ -40,17 +45,12 @@ class ItemController: UITableViewController {
         // TERNARY OPERATOR:
         // value = condition ? valueIfTrue : valueIfFalse
         cell.accessoryType = item.done == true ? .checkmark : .none
-        
-        let formatter = DateFormatter()
-        formatter.dateFormat = "MMM, dd, YYYY"
-        
-        cell.detailTextLabel?.text = formatter.string(from: date)
-        
+             
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //selectedArray[indexPath.row].done = !selectedArray[indexPath.row].done
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         saveItems()
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -58,31 +58,7 @@ class ItemController: UITableViewController {
     // MARK: - Add New Item
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-        
         performSegue(withIdentifier: "goToAddItem", sender: self)
-
-        
-      //  var textField = UITextField()
-      //  let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
-      //
-      //  alert.addTextField { (alertTextField) in
-      //      alertTextField.placeholder = "Create a new item"
-      //      textField = alertTextField
-      //  }
-      //
-      //  let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-      //      let newItem = Item(context: self.context)
-      //      newItem.title = textField.text!
-      //      newItem.done = false
-      //      newItem.category = self.selectedCategory
-      //
-      //      self.itemArray.append(newItem)
-      //      self.saveItems()
-      //      print(newItem.category)
-      //  }
-      //
-      //  alert.addAction(action)
-      //  self.present(alert, animated: true, completion: nil)
     }
     
     // MARK: - Model Mamipulation Methods
@@ -95,8 +71,6 @@ class ItemController: UITableViewController {
         }
         tableView.reloadData()
     }
-    
-    
     
     // Funktion mit zwei Parametern: 01: request , 02: predicate
     // 01 vom Typ Fetch-Request-Objeckt vom Typ NSFetchRequest<HealthItem>, Standardwert = Healthitem.fetchRequest()
@@ -113,6 +87,15 @@ class ItemController: UITableViewController {
         }
         tableView.reloadData()
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToAddItem" {
+            if let AddController = segue.destination as? AddController {
+                AddController.selectedCategory = selectedCategory
+                AddController.itemArray = itemArray
+            }
+        }
+    }
 }
 
 
@@ -125,16 +108,12 @@ extension ItemController: UISearchBarDelegate {
         
         // erstellt ein Request
         let request: NSFetchRequest<Item> = Item.fetchRequest()
-        
         // Filter-Query: %@ = searchBar.text, suche nach Item dessen Titel %@ enthält
         let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
-        
         // Request + Filter-Query
         request.predicate = predicate
-        
         // Sortierung nach title
         request.sortDescriptors  = [NSSortDescriptor(key: "title", ascending: true)]
-        
         
         do {
             itemArray = try context.fetch(request)
